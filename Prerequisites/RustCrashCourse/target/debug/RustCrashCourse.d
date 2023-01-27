4c821b91b45033f50ff36d493f777620735d64fa
akshaydhayal <akshaydhayalkd99@gmail.com>
2023-01-28 01:40:22 +0530

    Initiual setup rust basics

---
+++ Prerequisites/RustCrashCourse/target/debug/RustCrashCourse.d
@@ -0,0 +1,1 @@
+C:\Users\akshay.dhayal\Desktop\Solana-Q1-2023\Prerequisites\RustCrashCourse\target\debug\RustCrashCourse.exe: C:\Users\akshay.dhayal\Desktop\Solana-Q1-2023\Prerequisites\RustCrashCourse\src\main.rs
---
+++ Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse.d
@@ -0,0 +1,5 @@
+C:\Users\akshay.dhayal\Desktop\Solana-Q1-2023\Prerequisites\RustCrashCourse\target\debug\deps\RustCrashCourse.exe: src\main.rs
+
+C:\Users\akshay.dhayal\Desktop\Solana-Q1-2023\Prerequisites\RustCrashCourse\target\debug\deps\RustCrashCourse.d: src\main.rs
+
+src\main.rs:
