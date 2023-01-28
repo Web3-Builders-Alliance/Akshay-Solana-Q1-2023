a590f89043b82cbcf20e29b75271269b5827fad0
akshaydhayal <akshaydhayalkd99@gmail.com>
2023-01-28 05:50:34 +0530

    array tuples added

--- Prerequisites/RustCrashCourse/src/array.rs
+++ Prerequisites/RustCrashCourse/src/array.rs
@@ -1,7 +1,7 @@
 pub fn run(){
     let mut arr:[i32;5]=[1,2,3,4,5]; //need exact 5 items, neither less nor more hence fixed sizes
 
-    arr[2]=0;  //modifying array items
+    arr[2]=0;  //modifying array item
     println!("Array : {:?} , array len : {},  array[1] : {}, 
     bytes occupied : {}",arr,arr.len(), arr[1], std::mem::size_of_val(&arr));
 
--- Prerequisites/RustCrashCourse/src/tuple.rs
+++ Prerequisites/RustCrashCourse/src/tuple.rs
@@ -1,6 +1,6 @@
 //tuple group diff datatypes elements, max 12 elements
 pub fn run(){
-    let a:(&str, &str, i32)=("Ram","Kumar",20);   //&str is string literals
+    let a:(&str, &str, i32)=("Ram","Kumar",20);   //&str is string literalss
 
     println!("{} {} is {} years old",a.0,a.1,a.2);  //accessing tuple items
 }
